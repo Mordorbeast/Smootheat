6af2d288fec5841dd8982046f19dae22fed0c4ae
Xavi Gimenez <xavilongogimenez@gmail.com>
2019-02-15 17:16:52 +0100

    Creo pantalla menu principal

---
+++ app/src/main/java/com/example/xavi/proyectoxavigimenez/Pantalla_MenuPrincipal.kt
@@ -0,0 +1,36 @@
+package com.example.xavi.proyectoxavigimenez
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_pantalla__menu_principal.*
+
+class Pantalla_MenuPrincipal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla__menu_principal)
+
+        boton_recetas.setOnClickListener{
+            val intent1 = Intent(this, Pantalla_recetas::class.java)
+            startActivityForResult(intent1,1)
+        }
+
+        boton_nevera.setOnClickListener{
+            val intent2 = Intent(this, Pantalla_lista_compra::class.java)
+            startActivityForResult(intent2,1)
+        }
+
+        boton_como_cocinar.setOnClickListener{
+            val intent3 = Intent(this, Pantalla_lista_compra::class.java)
+            startActivityForResult(intent3,1)
+        }
+
+        boton_lista_compra.setOnClickListener{
+            val intent4 = Intent(this, Pantalla_lista_compra::class.java)
+            startActivityForResult(intent4,1)
+        }
+
+
+    }
+}
