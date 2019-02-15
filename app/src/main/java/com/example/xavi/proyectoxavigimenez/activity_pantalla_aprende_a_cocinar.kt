5db5405e32dbce38dd10cc1a28faff9f86c651b1
Xavi Gimenez <xavilongogimenez@gmail.com>
2019-02-15 17:29:31 +0100

    Creo pantalla aprende a cocinar

---
+++ app/src/main/java/com/example/xavi/proyectoxavigimenez/activity_pantalla_aprende_a_cocinar.kt
@@ -0,0 +1,12 @@
+package com.example.xavi.proyectoxavigimenez
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_pantalla_aprende_a_cocinar : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_aprende_a_cocinar)
+    }
+}
